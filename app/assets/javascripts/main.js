da79023c892856e7544241eb97deb2b7ce3794a1
Noe <noeon270@hotmail.com>
2016-04-12 12:45:26 -0500

    add in lightbox files

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,7 +1,11 @@
-$(function () {
-    $('.tlt').textillate({ in: { effect: 'fadeInRight' }, 
-    loop: true, 
-    out: {effect: 'fadeOutLeft', reverse:true},
-    minDisplayTime: 3500
-  });
-})
+$(document).on('ready page:load', ready);
+function ready() {
+  
+  $(function () {
+      $('.tlt').textillate({ in: { effect: 'fadeInRight' }, 
+      loop: true, 
+      out: {effect: 'fadeOutLeft', reverse:true},
+      minDisplayTime: 3500
+    });
+  })
+}
--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,4 +1,16 @@
 class MainController < ApplicationController
   def index
   end
+  
+  def products
+  end
+  
+  def gallery
+  end
+  
+  def about
+  end
+  
+  def contact
+  end
 end
