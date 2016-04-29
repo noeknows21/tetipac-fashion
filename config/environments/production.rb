b86e84cbd7f5d495f0ef8a71727fd8ac307f4e80
Noe <noeon270@hotmail.com>
2016-04-29 17:21:52 -0500

    contact phone number and production 1

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -13,6 +13,18 @@ Rails.application.configure do
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
+  
+  config.action_mailer.raise_delivery_errors = true
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'gmail.com',
+    user_name:            ENV["GMAIL_TETI_USER"],
+    password:             ENV["GMAIL_TETI_PW"],
+    authentication:       'login',
+    enable_starttls_auto: true  }
 
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
   # Add `rack-cache` to your Gemfile before enabling this.
